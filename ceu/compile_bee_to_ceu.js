e98611494e01eb63f959655dc0a5fd9504a172b2
Nate Morse <morse.nate@gmail.com>
2015-05-17 10:39:29 -0400

    Create compile_bee_to_ceu.js

---
+++ ceu/compile_bee_to_ceu.js
@@ -0,0 +1,13 @@
+// compile a BeeGraph to a ceu program.
+// convert BeeGraph nodes to ceu blocks awakened by messages.
+// convert BeeGraph edges to cue blocks that emit events.
+
+module.b2c = function() {
+  var compile = function (a) {
+    var ceu_src = '';
+    // convert BeeGraph to ceu
+    
+    return ceu_src;
+  };
+  return {"compile":compile};
+};
